38ebd3c96c6e61ad89e4f6bd0500ee2883bd8bc2
MLRichter <matrichter@uos.de>
2023-09-16 12:52:42 -0400

    finalized hyperparameters

--- salvo_launcher_generate_dataset_lc.sh
+++ salvo_launcher_generate_dataset_lc.sh
@@ -7,12 +7,12 @@ script_to_execute="./generate_dataset.sh"
 arg1=0
 
 # Second argument is always 500
-arg2=5000
+arg2=1000
 
 # Loop from 0 to 29500 with steps of 500
-while [ $arg1 -le 5000 ]; do
+while [ $arg1 -le 9000 ]; do
     # Execute the other script with the arguments
     sbatch $script_to_execute $arg1 $arg2 $1
     # Increment the first argument by 500
-    arg1=$((arg1 + 5000))
+    arg1=$((arg1 + 1000))
 done
