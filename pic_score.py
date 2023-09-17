aab7735946c816269579fcf9e7fb8b14e136daa4
MLRichter <matrichter@uos.de>
2023-09-16 23:03:08 -0400

    finalized hyperparameters

--- pic_score.py
+++ pic_score.py
@@ -152,8 +152,9 @@ def main(dataset1: str, dataset2: str, parquet_file: str = "../coco2017/coco_30k
 if __name__ == '__main__':
     #parquet_file = "../coco2017/long_context_val.parquet"
     parquet_file = "../coco2017/coco_30k.parquet"
-    dataset1 = "output/wuerstchen_long_context_generated"
+    dataset1 = "output/wuerstchen_generated"
     datasets2 = [
+        "output/sdxl_generated",
         "output/sd14_generated",
         "output/sd21_generated",
         "output/ldm14_generated",
