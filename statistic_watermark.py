99d23f3d3bb5ce0b216f83d2c541db48f2b6571e
MLRichter <matrichter@uos.de>
2023-10-28 06:58:37 -0400

    finalized hyperparameters

--- statistic_watermark.py
+++ statistic_watermark.py
@@ -1,4 +1,3 @@
-import json
 import warnings
 
 import torch
@@ -25,63 +24,6 @@ from torchtools.utils.diffusion2 import DDPMSampler
 from ld_model import LDM
 from utils import WebdatasetFilter
 
-class WebdatasetFilterCounter():
-    def __init__(self, min_size=512, max_pwatermark=0.5, aesthetic_threshold=5.0, unsafe_threshold=0.99, text_conditions=None): # {'min_words': 2, 'forbidden_words': ["www.", ".com", "http", "-", "_", ":", ";", "(", ")", "/", "%", "|", "?", "download", "interior", "kitchen", "chair", "getty", "how", "what", "when", "why", "laminate", "furniture", "hair", "dress", "clothing"]}):
-        self.min_size = min_size
-        self.max_pwatermark = max_pwatermark
-        self.aesthetic_threshold = aesthetic_threshold
-        self.unsafe_threshold = unsafe_threshold
-        self.text_conditions = text_conditions
-
-        self.f_watermark = 0
-        self.f_aesthetic = 0
-        self.f_unsafe = 0
-        self.f_size = 0
-        self.total = 0
-
-    def update_counters(self, filter_watermark, filter_aesthetics_a, filter_aesthetics_b, filter_unsafe, filter_text):
-        self.f_watermark += filter_watermark
-        self.f_aesthetic += (filter_aesthetics_a or filter_aesthetics_b)
-        self.f_unsafe += filter_unsafe
-        self.f_size += filter_text
-        self.total += 1
-        if self.total%10 == 0:
-            self.checkpoint(savepath="../stats/filter_stats_{}.json")
-
-    def checkpoint(self, savepath: str):
-        with open(savepath.format(self.total), "w") as fp:
-            json.dump({
-                'f_watermark': self.f_watermark,
-                'f_aesthetic': self.f_aesthetic,
-                'f_unsafe': self.f_unsafe,
-                'f_size': self.f_size,
-                'total': self.total,
-            }, fp)
-
-    def __call__(self, x):
-        try:
-            if 'json' in x:
-                x_json = json.loads(x['json'])
-                filter_size = (x_json.get('original_width', 0.0) or 0.0) >= self.min_size and x_json.get('original_height', 0) >= self.min_size
-                filter_watermark = (x_json.get('pwatermark', 1.0) or 1.0) <= self.max_pwatermark
-                filter_aesthetic_a = (x_json.get('aesthetic', 0.0) or 0.0) >= self.aesthetic_threshold
-                filter_aesthetic_b = (x_json.get('AESTHETIC_SCORE', 0.0) or 0.0) >= self.aesthetic_threshold
-                filter_unsafe = (x_json.get('punsafe', 1.0) or 1.0) <= self.unsafe_threshold
-                if self.text_conditions is not None:
-                    caption = x['txt'].decode("utf-8")
-                    filter_min_words = len(caption.split(" ")) >= self.text_conditions['min_words']
-                    filter_ord_128 = all([ord(c) < 128 for c in caption])
-                    filter_forbidden_words = all([c not in caption.lower() for c in self.text_conditions['forbidden_words']])
-                    filter_text = filter_min_words and filter_ord_128 and filter_forbidden_words
-                else:
-                    filter_text = True
-                return filter_size and filter_watermark and (filter_aesthetic_a or filter_aesthetic_b) and filter_unsafe and filter_text
-            else:
-                return False
-        except:
-            return False
-
-
 # PARAMETERS
 updates = 300000  # 500000
 warmup_updates = 10000
@@ -91,9 +33,25 @@ max_iters = updates * grad_accum_steps
 print_every = 10 * grad_accum_steps
 lr = 1e-4
 
+dataset_path = "pipe:aws s3 cp s3://stability-west/laion-a-native-high-res/{part-0/{00000..18699}.tar,part-1/{00000..18699}.tar,part-2/{00000..18699}.tar,part-3/{00000..18699}.tar,part-4/{00000..18699}.tar} -"  # "pipe:aws s3 cp s3://laion-west/humans-7M-with-blip-caps+aesthetics+nsfw/00000{1..5499}.tar -"
 dataset_path = "pipe:aws s3 cp s3://stability-west/laion-a-native-high-res/{part-0/{00000..18000}.tar,part-1/{00000..13500}.tar,part-2/{00000..13500}.tar,part-3/{00000..13500}.tar,part-4/{00000..14100}.tar} -"  # "pipe:aws s3 cp s3://laion-west/humans-7M-with-blip-caps+aesthetics+nsfw/00000{1..5499}.tar -"
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
+clip_image_model_name = 'laion/CLIP-ViT-H-14-laion2B-s32B-b79K'
+output_path = "../output/baseline/exp1/"
+checkpoint_path = "../models/baseline/exp1.pt"
+target = "e"
+
+wandv_project = "LDBaseline"
+wandv_entity = "mlrichter"
+wandb_run_name = "LDTrain"
+wandb_config = {
+    "model_type": 'Latent Diffusion 0.87M',
+    "target": f'{target}-target',
+    "image_size": "512x512",
+    "batch_size": batch_size,
+    "warmup_updates": warmup_updates,
+    "lr": lr,
+    "description": "Latent Diffusion Model Similar to SD-1.4"
+}
 
 magic_norm = 0.18215
 transforms = torchvision.transforms.Compose([
@@ -107,45 +65,24 @@ transforms = torchvision.transforms.Compose([
 def identity(x):
     return x
 
-
-max_pwatermark=0.5
-aesthetic_threshold=5.0
-unsafe_threshold=0.99
-min_size = 512
-
-
-filter_counter = WebdatasetFilterCounter(
-    min_size=min_size, max_pwatermark=max_pwatermark, aesthetic_threshold=aesthetic_threshold,
-    unsafe_threshold=unsafe_threshold)
-
+# --- PREPARE DATASET ---
+# PREPARE DATASET
 dataset = wds.WebDataset(
-    dataset_path, resampled=True, handler=warn_and_continue
+        dataset_path, resampled=True, handler=warn_and_continue
 ).select(
-    WebdatasetFilter(min_size=512, max_pwatermark=0.5, aesthetic_threshold=5.0, unsafe_threshold=0.99)
+        WebdatasetFilter(min_size=512, max_pwatermark=0.5, aesthetic_threshold=5.0, unsafe_threshold=0.99)
 ).shuffle(690, handler=warn_and_continue).decode(
-    "pilrgb", handler=warn_and_continue
+        "pilrgb", handler=warn_and_continue
 ).to_tuple(
-    "jpg", "txt", handler=warn_and_continue
+        "jpg", "txt", handler=warn_and_continue
 ).map_tuple(
-    transforms, identity, handler=warn_and_continue
+        transforms, identity, handler=warn_and_continue
 )
-print("created dataset")
-real_batch_size = 1
-
-
-dataloader = DataLoader(dataset, batch_size=real_batch_size, num_workers=0, pin_memory=True)
-print("created dataloader")
-
-
+real_batch_size = 256
+dataloader = DataLoader(dataset, batch_size=real_batch_size, num_workers=8, pin_memory=True)
+print("prepping dataloader")
+#dataloader_iterator = dataloader
 dataloader_iterator = iter(dataloader)
-print("entering primary loop")
-for i, _ in enumerate((dataloader_iterator)):
-    print(i)
 
-    if i > 1000:
-        break
-    #for xi in x:
-    #    filter_counter(x)
-    if i%100 == 0:
-        filter_counter.checkpoint(savepath="../stats/filter_stats_{}.json")
-
+for i, x in enumerate(dataloader_iterator):
+    print(i, x)
