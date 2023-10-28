4d4056fa0b457defd9be27c5b5e545699af1b0b4
MLRichter <matrichter@uos.de>
2023-10-28 07:21:26 -0400

    finalized hyperparameters

--- statistic_watermark.py
+++ statistic_watermark.py
@@ -61,6 +61,63 @@ transforms = torchvision.transforms.Compose([
     torchvision.transforms.Normalize([0.5], [0.5]),
 
 ])
+import json
+class WebdatasetFilterCounter():
+    def __init__(self, min_size=512, max_pwatermark=0.5, aesthetic_threshold=5.0, unsafe_threshold=0.99, text_conditions=None): # {'min_words': 2, 'forbidden_words': ["www.", ".com", "http", "-", "_", ":", ";", "(", ")", "/", "%", "|", "?", "download", "interior", "kitchen", "chair", "getty", "how", "what", "when", "why", "laminate", "furniture", "hair", "dress", "clothing"]}):
+        self.min_size = min_size
+        self.max_pwatermark = max_pwatermark
+        self.aesthetic_threshold = aesthetic_threshold
+        self.unsafe_threshold = unsafe_threshold
+        self.text_conditions = text_conditions
+
+        self.f_watermark = 0
+        self.f_aesthetic = 0
+        self.f_unsafe = 0
+        self.f_size = 0
+        self.total = 0
+
+    def update_counters(self, filter_watermark, filter_aesthetics_a, filter_aesthetics_b, filter_unsafe, filter_text):
+        self.f_watermark += filter_watermark
+        self.f_aesthetic += (filter_aesthetics_a or filter_aesthetics_b)
+        self.f_unsafe += filter_unsafe
+        self.f_size += filter_text
+        self.total += 1
+        if self.total%10 == 0:
+            self.checkpoint(savepath="../stats/filter_stats_{}.json")
+
+    def checkpoint(self, savepath: str):
+        with open(savepath.format(self.total), "w") as fp:
+            json.dump({
+                'f_watermark': self.f_watermark,
+                'f_aesthetic': self.f_aesthetic,
+                'f_unsafe': self.f_unsafe,
+                'f_size': self.f_size,
+                'total': self.total,
+            }, fp)
+
+    def __call__(self, x):
+        try:
+            if 'json' in x:
+                x_json = json.loads(x['json'])
+                filter_size = (x_json.get('original_width', 0.0) or 0.0) >= self.min_size and x_json.get('original_height', 0) >= self.min_size
+                filter_watermark = (x_json.get('pwatermark', 1.0) or 1.0) <= self.max_pwatermark
+                filter_aesthetic_a = (x_json.get('aesthetic', 0.0) or 0.0) >= self.aesthetic_threshold
+                filter_aesthetic_b = (x_json.get('AESTHETIC_SCORE', 0.0) or 0.0) >= self.aesthetic_threshold
+                filter_unsafe = (x_json.get('punsafe', 1.0) or 1.0) <= self.unsafe_threshold
+                if self.text_conditions is not None:
+                    caption = x['txt'].decode("utf-8")
+                    filter_min_words = len(caption.split(" ")) >= self.text_conditions['min_words']
+                    filter_ord_128 = all([ord(c) < 128 for c in caption])
+                    filter_forbidden_words = all([c not in caption.lower() for c in self.text_conditions['forbidden_words']])
+                    filter_text = filter_min_words and filter_ord_128 and filter_forbidden_words
+                else:
+                    filter_text = True
+                return filter_size and filter_watermark and (filter_aesthetic_a or filter_aesthetic_b) and filter_unsafe and filter_text
+            else:
+                return False
+        except:
+            return False
+
 
 def identity(x):
     return x
@@ -84,6 +141,8 @@ print("prepping dataloader")
 #dataloader_iterator = dataloader
 dataloader_iterator = iter(dataloader)
 
+counter = WebdatasetFilterCounter(min_size=512, max_pwatermark=0.5, aesthetic_threshold=5.0, unsafe_threshold=0.99)
 for i, x in enumerate(tqdm(dataset)):
+    counter({"json": x[1]})
     if i%5 == 0:
         print(i, x)
